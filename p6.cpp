43e4520bc25c2a4ed9b9519f564b01591b416c86
Sharan S poojari <117771260+Sharanpoojarissp@users.noreply.github.com>
2023-08-27 10:43:48 +0530

    Add files via upload

---
+++ p6.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int n = 4;
+
+    for(int i= 1 ; i<=n ; i++ ){
+        // for (int j = i; j <= n ; j++){
+        for (int j = 1 ; j <= n-i+1 ; j++){
+            cout << j  <<" "  ; 
+        }
+        cout<< endl;
+    }
+    return 0;
+}
